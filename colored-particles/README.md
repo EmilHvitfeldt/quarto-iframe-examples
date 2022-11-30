90e2e1e78984f6c9ad6487f8ef03845be9015a2a
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-11-30 10:47:31 -0800

    add colored particles

---
+++ colored-particles/README.md
@@ -0,0 +1,5 @@
+# tsParticles - Slack Colored Particles
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/matteobruni/pen/ZExLYyg](https://codepen.io/matteobruni/pen/ZExLYyg).
+
+- removed github buttons
---
+++ colored-particles/script.js
@@ -0,0 +1,61 @@
+tsParticles.load({
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        area: 800
+      }
+    },
+    color: {
+      value: ["#2EB67D", "#ECB22E", "#E01E5B", "#36C5F0"]
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 1
+    },
+    size: {
+      value: { min: 1, max: 8 }
+    },
+    links: {
+      enable: true,
+      distance: 150,
+      color: "#808080",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: "none",
+      random: false,
+      straight: false,
+      outModes: "out"
+    }
+  },
+  interactivity: {
+    events: {
+      onHover: {
+        enable: true,
+        mode: "grab"
+      },
+      onClick: {
+        enable: true,
+        mode: "push"
+      }
+    },
+    modes: {
+      grab: {
+        distance: 140,
+        links: {
+          opacity: 1
+        }
+      },
+      push: {
+        quantity: 4
+      }
+    }
+  }
+});
